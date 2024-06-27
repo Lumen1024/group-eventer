f62c8043f00e936538ee70a87a3973906723d430
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-06-27 22:01:42 +0700

    edge to edge added

--- app/src/main/java/com/lumen1024/groupeventer/app/MainActivity.kt
+++ app/src/main/java/com/lumen1024/groupeventer/app/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.lumen1024.groupeventer.app
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.lumen1024.groupeventer.app.navigation.MainNavGraph
 import com.lumen1024.groupeventer.shared.model.GroupEventerTheme
@@ -13,7 +16,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-//        enableEdgeToEdge()
+        // make top and bottom bar transparent
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
         setContent {
             GroupEventerTheme {
                 MainNavGraph()
--- app/src/main/java/com/lumen1024/groupeventer/entities/group/model/Group.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/group/model/Group.kt
@@ -1,9 +1,11 @@
 package com.lumen1024.groupeventer.entities.group.model
 
+import com.google.firebase.firestore.DocumentId
 import com.lumen1024.groupeventer.entities.group_event.model.GroupEvent
 
 data class Group(
-    val id: String = "",
+    @DocumentId
+    val id: String? = null,
 
     val events: List<GroupEvent> = emptyList(),
     val people: List<Long> = emptyList()
