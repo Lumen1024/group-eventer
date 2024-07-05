8d470ba405f988918c2df085e2687a0e015c74f6
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-07-06 02:54:38 +0700

    ref: add @Singleton annotation to modules

--- app/src/main/java/com/lumen1024/groupeventer/entities/hilt/model/AuthModule.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/hilt/model/AuthModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AuthModule {
 
+    @Singleton
     @Binds
     abstract fun bindAuthService(service: FirebaseAuthService): AuthService
 }
--- app/src/main/java/com/lumen1024/groupeventer/entities/hilt/model/GroupModule.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/hilt/model/GroupModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class GroupModule {
 
+    @Singleton
     @Binds
     abstract fun bindGroupRepository(groupRepositoryImp: FirebaseGroupRepository): GroupRepository
 }
