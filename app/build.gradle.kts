f67cad5cd63675e77c37a8c49d427510b16d2161
MangriMen <49300253+MangriMen@users.noreply.github.com>
2024-07-12 22:05:49 +0700

    feat: bump kotlin version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 
+    alias(libs.plugins.compose.compiler)
+
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.google.services)
@@ -57,9 +59,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
