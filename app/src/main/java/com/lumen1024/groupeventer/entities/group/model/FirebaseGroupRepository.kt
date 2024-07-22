55796df9f24977a5a05b2ed9e74c813b9caad79a
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-07-22 10:40:28 +0700

    ref: [WIP] GroupRepository

--- app/src/main/java/com/lumen1024/groupeventer/entities/group/model/FirebaseGroupRepository.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/group/model/FirebaseGroupRepository.kt
@@ -1,12 +1,8 @@
 package com.lumen1024.groupeventer.entities.group.model
 
-import android.util.Log
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.firestore
-import com.lumen1024.groupeventer.shared.model.RepositoryException
 import com.lumen1024.groupeventer.shared.model.RepositoryObjectChange
-import com.lumen1024.groupeventer.shared.model.toRepositoryException
 import com.lumen1024.groupeventer.shared.model.toRepositoryObjectChange
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -14,168 +10,119 @@ import javax.inject.Inject
 class FirebaseGroupRepository @Inject constructor(
     firebase: Firebase,
 ) : GroupRepository {
+
     private val collection = firebase.firestore.collection("groups")
 
     override suspend fun add(group: Group): Result<Unit> {
-        collection.document(group.id).set(group).await()
-        return Result.success(Unit)
+        try {
+            collection.document(group.id).set(group).await()
+            return Result.success(Unit)
+
+        } catch (e: Throwable) {
+            return Result.failure(e)
+        }
     }
 
     override suspend fun get(id: String): Result<Group> {
         try {
             val group = collection
                 .whereEqualTo(Group::id.name, id)
-                .get()
-                .await().toObjects(GroupDto::class.java)[0].toGroup()
-
-            if (group == null) {
-                return Result.failure(
-                    GroupRepositoryException(
-                        RepositoryException.Code.UNKNOWN,
-                        "Group is null"
-                    )
-                )
-            }
+                .get().await()
+                .documents.getOrNull(0)
+                ?.toObject(GroupDto::class.java)
+                ?.toGroup()
+                ?: return Result.failure(Throwable("No group with this id"))
 
             return Result.success(group)
-        } catch (e: Exception) {
-            Log.e("repository", e.message, e)
-            if (e is FirebaseFirestoreException) {
-                return Result.failure(e.toRepositoryException())
-            }
 
-            return Result.failure(
-                GroupRepositoryException(
-                    RepositoryException.Code.UNKNOWN,
-                    e.message
-                )
-            )
+        } catch (e: Throwable) {
+            return Result.failure(e)
         }
     }
 
     override suspend fun get(name: String, password: String?): Result<Group> {
         val query = collection
-            .whereEqualTo("name", name)
-            .let {
-                return@let if (!password.isNullOrEmpty()) it.whereEqualTo(
-                    "password",
-                    password
-                ) else it
-            }
-            .get()
-            .await()
+            .whereEqualTo(GroupDto::name.name, name)
+            .whereEqualTo(GroupDto::password.name, password)
 
-        if (query.isEmpty)
-            return Result.failure(
-                GroupRepositoryException(
-                    code = RepositoryException.Code.NOT_FOUND,
-                    message = "" // todo
-                )
-            )
+        try {
+            val group = query
+                .get().await()
+                .documents.getOrNull(0)
+                ?.toObject(GroupDto::class.java)?.toGroup()
+                ?: return Result.failure(Throwable("No group with this credits"))
+            return Result.success(group)
 
-        val group = query.documents[0].toObject(GroupDto::class.java)!!.toGroup() // up check
-        return Result.success(group)
+        } catch (e: Throwable) {
+            return Result.failure(e)
+        }
     }
 
     override suspend fun getList(ids: List<String>): Result<List<Group>> {
-        try {
-            val groupsQuery =
-                if (ids.isNotEmpty()) collection.whereIn("id", ids)
-                else collection
+        val query =
+            if (ids.isNotEmpty()) collection.whereIn("id", ids)
+            else collection
 
-            val groups = groupsQuery
-                .get()
-                .await()
-            return Result.success(groups.toObjects(GroupDto::class.java).map { it.toGroup() })
-        } catch (e: Exception) {
-            Log.e("repository", e.message, e)
-            if (e is FirebaseFirestoreException) {
-                return Result.failure(e.toRepositoryException())
-            }
+        return try {
+            val groups = query
+                .get().await()
+                .toObjects(GroupDto::class.java).map { it.toGroup() }
+            Result.success(groups)
 
-            return Result.failure(
-                GroupRepositoryException(
-                    RepositoryException.Code.UNKNOWN,
-                    e.message
-                )
-            )
+        } catch (e: Throwable) {
+            Result.failure(e)
         }
     }
 
     override suspend fun update(
-        groupId: String,
+        id: String,
         data: Map<String, Any>,
     ): Result<Unit> {
         return try {
-            Result.success(
-                collection
-                    .document(groupId)
-                    .update(data)
-                    .await()
-            )
-        } catch (e: Exception) {
-            Log.e("repository", e.message, e)
-            if (e is FirebaseFirestoreException) {
-                return Result.failure(e.toRepositoryException())
-            }
+            collection.document(id).update(data)
+                .await()
+            Result.success(Unit)
 
-            return Result.failure(
-                GroupRepositoryException(
-                    RepositoryException.Code.UNKNOWN,
-                    e.message
-                )
-            )
+        } catch (e: Throwable) {
+            Result.failure(e)
         }
     }
 
-    override suspend fun removeGroup(groupId: String): Result<Void> {
+    override suspend fun remove(id: String): Result<Unit> {
         return try {
-            Result.success(
-                collection
-                    .document(groupId)
-                    .delete()
-                    .await()
-            )
-        } catch (e: Exception) {
-            Log.e("repository", e.message, e)
-            if (e is FirebaseFirestoreException) {
-                return Result.failure(e.toRepositoryException())
-            }
+            collection
+                .document(id).delete()
+                .await()
+            Result.success(Unit)
 
-            return Result.failure(
-                GroupRepositoryException(
-                    RepositoryException.Code.UNKNOWN,
-                    e.message
-                )
-            )
+        } catch (e: Throwable) {
+            Result.failure(e)
         }
     }
 
     override fun listenList(
-        groupIds: List<String>,
+        ids: List<String>,
         callback: (List<RepositoryObjectChange<Group?>>) -> Unit,
     ): Result<Unit> {
-        if (groupIds.isEmpty()) {
-            throw Exception("Ids list must not be empty")
+
+        if (ids.isEmpty()) {
+            return Result.failure(Throwable("Ids must not be empty"))
         }
 
-        val registration = collection.whereIn("id", groupIds).addSnapshotListener { snapshot, e ->
-            if (e !== null) {
-                return@addSnapshotListener
-            }
+        val query = collection.whereIn("id", ids)
 
-            callback(snapshot!!.documentChanges.map { it ->
-                val change = it.toRepositoryObjectChange<GroupDto>()
-                val mappedChane =
-                    RepositoryObjectChange(change.type, change.data?.toGroup())
-                return@map mappedChane
-            })
+        query.addSnapshotListener { snapshot, e ->
+            if (e != null) return@addSnapshotListener
+
+            val changes = snapshot?.documentChanges?.map { firebaseChange ->
+                val changeDto = firebaseChange.toRepositoryObjectChange<GroupDto>()
+                RepositoryObjectChange(changeDto.type, changeDto.data?.toGroup())
+
+            } ?: return@addSnapshotListener
+
+            callback(changes)
         }
 
         return Result.success(Unit) // maybe callback
-    }
-
-    override fun listen(groupId: String, callback: (Group) -> Unit) {
-        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/lumen1024/groupeventer/entities/group/model/GroupRepository.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/group/model/GroupRepository.kt
@@ -7,22 +7,22 @@ interface GroupRepository {
     suspend fun add(group: Group) : Result<Unit>
 
 
-    suspend fun get(name: String, password: String? = null) : Result<Group>
     suspend fun get(id: String) : Result<Group>
+    suspend fun get(name: String, password: String? = null) : Result<Group>
 
-    suspend fun getList(properties: Map<String, Any> = emptyMap()): Result<List<Group>>
+    //suspend fun getList(properties: Map<String, Any> = emptyMap()): Result<List<Group>>
     suspend fun getList(ids: List<String> = emptyList()): Result<List<Group>>
 
 
-    suspend fun update(groupId: String, data: Map<String, Any>): Result<Unit>
+    suspend fun update(id: String, data: Map<String, Any>): Result<Unit>
 
-    suspend fun remove(groupId: String): Result<Unit>
+    suspend fun remove(id: String): Result<Unit>
 
 
     fun listenList(
-        groupIds: List<String>,
+        ids: List<String>,
         callback: (List<RepositoryObjectChange<Group?>>) -> Unit,
     ): Result<Unit>
 
-    fun listen(groupId: String, callback: (Group) -> Unit) : Result<Unit>
+    //fun listen(id: String, callback: (Group) -> Unit) : Result<Unit>
 }
