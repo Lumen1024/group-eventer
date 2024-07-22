52addb9b6affbcc9711c46088fb31c750263cc60
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-07-22 10:40:54 +0700

    ref: [WIP] Auth Service

--- app/src/main/java/com/lumen1024/groupeventer/entities/auth/model/FirebaseAuthService.kt
+++ app/src/main/java/com/lumen1024/groupeventer/entities/auth/model/FirebaseAuthService.kt
@@ -1,119 +1,49 @@
 package com.lumen1024.groupeventer.entities.auth.model
 
-import android.net.Uri
-import android.util.Log
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.auth
-import com.google.firebase.auth.userProfileChangeRequest
-import com.lumen1024.groupeventer.entities.user_data.model.User
-import com.lumen1024.groupeventer.entities.user_data.model.toUser
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class FirebaseAuthService @Inject constructor(
-    private val firebase: Firebase,
+    firebase: Firebase,
 ) : AuthService {
 
-    private val listeners = mutableSetOf<(FirebaseAuth) -> Unit>()
+    private val auth = firebase.auth
 
-    override fun checkAuthorized(): Boolean {
-        return firebase.auth.currentUser != null
-    }
-
-    override suspend fun updateUser(name: String?, avatarUrl: Uri?): Result<Unit> {
-        try {
-            val profileUpdates = UserProfileChangeRequest.Builder()
-
-            if (name !== null) {
-                profileUpdates.displayName = name
-            }
-
-            if (avatarUrl !== null) {
-                profileUpdates.photoUri = avatarUrl
-            }
-
-            Firebase.auth.currentUser?.updateProfile(profileUpdates.build())?.await()
-
-            this.updateUserAndTriggerListeners()
-
-            return Result.success(Unit)
-        } catch (e: Exception) {
-            Log.e("service", e.message, e)
-            return if (e is FirebaseAuthException)
-                Result.failure(e.toAuthException())
-            else
-                Result.failure(AuthException.Unknown(""))
-        }
+    override fun checkAuthorized(): Result<Boolean> {
+        return Result.success(auth.currentUser != null)
     }
 
     override suspend fun login(email: String, password: String): Result<Unit> {
         try {
-            Firebase.auth.signInWithEmailAndPassword(email, password).await()
+            auth.signInWithEmailAndPassword(email, password).await()
             return Result.success(Unit)
         } catch (e: Exception) {
-            Log.e("service", e.message, e)
             return if (e is FirebaseAuthException)
                 Result.failure(e.toAuthException())
             else
-                Result.failure(AuthException.Unknown(""))
+                Result.failure(e)
         }
     }
 
-    override suspend fun register(
-        email: String,
-        name: String,
-        password: String,
-    ): Result<Unit> {
-
+    override suspend fun register(email: String, password: String): Result<Unit> {
         try {
-            firebase.auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                val profileUpdates = userProfileChangeRequest {
-                    displayName = name
-                }
-
-                it.result.user?.updateProfile(profileUpdates)
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
             }.await()
 
-            this.updateUserAndTriggerListeners()
-
             return Result.success(Unit)
         } catch (e: Exception) {
-            Log.e("service", e.message, e)
             return if (e is FirebaseAuthException)
                 Result.failure(e.toAuthException())
             else
-                Result.failure(AuthException.Unknown(""))
+                Result.failure(e)
         }
     }
 
-    override suspend fun logout() {
-        firebase.auth.signOut()
-    }
-
-    override fun listenChanges(callback: (User?) -> Unit): () -> Unit {
-        val listener: (auth: FirebaseAuth) -> Unit = {
-            callback(it.currentUser?.toUser())
-        }
-
-        Firebase.auth.addAuthStateListener(listener)
-        listeners.add(listener)
-
-        listener(Firebase.auth)
-
-        return {
-            Firebase.auth.removeAuthStateListener(listener)
-            listeners.remove(listener)
-        }
-    }
-
-    private suspend fun updateUserAndTriggerListeners() {
-        Firebase.auth.currentUser?.reload()?.await()
-
-        for (listener in listeners) {
-            listener(Firebase.auth)
-        }
+    override suspend fun logout(): Result<Unit> {
+        auth.signOut()
+        return Result.success(Unit)
     }
 }
