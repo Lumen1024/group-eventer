2c28433227c001a3624308476ab61339602a6808
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-08-04 07:06:59 +0700

    docs: flatMapLinked

--- app/src/main/java/com/lumen1024/groupeventer/shared/lib/flatMapLinked.kt
+++ app/src/main/java/com/lumen1024/groupeventer/shared/lib/flatMapLinked.kt
@@ -1,5 +1,9 @@
 package com.lumen1024.groupeventer.shared.lib
 
+/**
+ * Returns a single map of all elements in keys and parent in value
+ * yielded from results of [transform] function being invoked on each element of original collection.
+ */
 inline fun <T, R> Iterable<R>.flatMapLinked(transform: (R) -> Iterable<T>): Map<T, R> {
     return flatMap { parent ->
         transform(parent).map { item ->
