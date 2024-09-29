07954472c8e67b9f93c6e2d6eab949552b01deb2
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-09-29 20:29:34 +0700

    ref: Navigator.kt

--- presentation/src/main/java/com/lumen1024/presentation/Navigator.kt
+++ presentation/src/main/java/com/lumen1024/presentation/Navigator.kt
@@ -1,15 +1,16 @@
 package com.lumen1024.presentation
 
 import androidx.navigation.NavOptionsBuilder
+import com.lumen1024.presentation.screen.Screen
 import kotlinx.coroutines.channels.Channel
 
 interface Navigator {
     val navigationChannel: Channel<NavigationIntent>
     fun navigate(
-        screen: com.lumen1024.presentation.Screen,
+        screen: Screen,
         builder: NavOptionsBuilder.() -> Unit = { }
     )
 
     fun back(inclusive: Boolean = false)
-    fun deepBack(screen: com.lumen1024.presentation.Screen, inclusive: Boolean = false)
+    fun deepBack(screen: Screen, inclusive: Boolean = false)
 }
