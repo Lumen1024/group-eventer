e44fe7354132598ae2e369accac6e8e76e9bd7f2
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-09-29 18:25:31 +0700

    ref: delete LocalNavController.kt

--- presentation/src/main/java/com/lumen1024/presentation/LocalNavController.kt
+++
@@ -1,7 +0,0 @@
-package com.lumen1024.presentation
-
-import androidx.compose.runtime.compositionLocalOf
-import androidx.navigation.NavHostController
-
-val LocalNavController =
-    compositionLocalOf<NavHostController> { error("No NavHostController found!") }
