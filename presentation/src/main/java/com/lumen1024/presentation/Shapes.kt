8cf06c009ab8359c1956127a067fb98d75571ef0
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-09-29 19:10:04 +0700

    ref: move Shapes.kt

--- presentation/src/main/java/com/lumen1024/presentation/Shapes.kt
+++ presentation/src/main/java/com/lumen1024/presentation/Shapes.kt
@@ -1,4 +1,4 @@
-package com.lumen1024.groupeventer.shared.model
+package com.lumen1024.presentation
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
