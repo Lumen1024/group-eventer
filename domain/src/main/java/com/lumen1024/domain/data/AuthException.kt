28c874d654e2c6cdc16d9ecc4c2790edd82f5c70
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-10-05 22:52:19 +0700

    fix: splash screen

--- domain/src/main/java/com/lumen1024/domain/data/AuthException.kt
+++ domain/src/main/java/com/lumen1024/domain/data/AuthException.kt
@@ -1,6 +1,6 @@
 package com.lumen1024.domain.data
 
-sealed class AuthException(message: String?) : Throwable(message) {
+sealed class AuthException(message: String?) : Exception(message) {
     class WrongFormatEmail(message: String?) : AuthException(message)
     class ShortPassword(message: String?) : AuthException(message)
     class IncorrectCredentials(message: String?) : AuthException(message)
