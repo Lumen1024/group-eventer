b0cb6ddc8a7cbdcd3d9e0b460017d78f17af7412
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-10-30 17:11:39 +0700

    feat: add ScalableBottomSheet.kt

---
+++ ui/src/main/java/com/lumen1024/ui/shared/ScalableBottomSheet.kt
@@ -0,0 +1,54 @@
+package com.lumen1024.ui.shared
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedBoxWithConstraintsScope")
+@Composable
+fun ScalableBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    content: @Composable (heightMaxFraction: Float) -> Unit,
+) {
+    var fullHeight by remember { mutableIntStateOf(0) }
+    var heightProgressFraction by remember { mutableFloatStateOf(0f) }
+
+    // TODO: maybe move it in a right way to something like
+    //  LaunchedEffect as it says in documentation
+    //  Try catch because requireOffset can throw an exception
+    //  if call it before first composition
+    try {
+        val offset = sheetState.requireOffset()
+        heightProgressFraction = 1 - (offset / fullHeight)
+    } catch (_: Exception) {
+    }
+
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier
+            .width(IntrinsicSize.Min)
+            .height(IntrinsicSize.Min),
+    ) {
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            fullHeight = constraints.maxHeight
+            content(heightProgressFraction)
+        }
+    }
+}
