d1250b0100d2dd1a1eb35f69b559666f8aa94437
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-08 09:47:09 +0700

    ref: RepositoryObjectChange.kt

--- domain/src/main/java/com/lumen1024/domain/data/AuthException.kt
+++ domain/src/main/java/com/lumen1024/domain/data/AuthException.kt
@@ -7,30 +7,3 @@ sealed class AuthException(message: String?) : Exception(message) {
     class EmailAlreadyExist(message: String?) : AuthException(message)
     class Unknown(message: String?) : AuthException(message)
 }
-
-//fun FirebaseAuthException.toAuthException(): AuthException {
-//    return when (this.errorCode) {
-//        "ERROR_INVALID_EMAIL" ->
-//            com.lumen1024.domain.data.AuthException.WrongFormatEmail(this.message)
-//
-//        "ERROR_WEAK_PASSWORD" ->
-//            com.lumen1024.domain.data.AuthException.ShortPassword(this.message)
-//
-//        "ERROR_EMAIL_ALREADY_IN_USE" -> com.lumen1024.domain.data.AuthException.EmailAlreadyExist(this.message)
-//
-//        "ERROR_INVALID_CREDENTIAL" ->
-//            com.lumen1024.domain.data.AuthException.IncorrectCredentials(this.message)
-//
-//        else -> com.lumen1024.domain.data.AuthException.Unknown(this.message)
-//    }
-//}
-
-//fun AuthException.mapToResource(): Int {
-//    return when (this) {
-//        is AuthException.WrongFormatEmail -> R.string.wrong_format_email
-//        is AuthException.IncorrectCredentials -> R.string.incorrect_credential
-//        is AuthException.ShortPassword -> R.string.short_password
-//        is AuthException.Unknown -> R.string.unknown_auth_exception
-//        is AuthException.EmailAlreadyExist -> R.string.email_already_exist
-//    }
-//}
