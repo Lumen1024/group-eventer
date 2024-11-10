53d43300c2a3b57763b8502e54c95c2e4bbf2b87
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-10 11:53:51 +0700

    ref: AvatarService.kt

--- domain/src/main/java/com/lumen1024/domain/service/AvatarService.kt
+++ domain/src/main/java/com/lumen1024/domain/service/AvatarService.kt
@@ -1,6 +1,5 @@
 package com.lumen1024.domain.service
 
 interface AvatarService {
-    suspend fun getAvatar(userId: String): Result<String?>
     suspend fun updateAvatar(userId: String, imageURI: String): Result<Unit>
 }
