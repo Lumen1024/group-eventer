d036a25c9fa011bb57e8628b441e3a9349018c72
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-10 11:38:39 +0700

    ref: GroupRepository.kt remove listen fun

--- data/src/main/java/com/lumen1024/data/implementation/repository/FirebaseGroupRepository.kt
+++ data/src/main/java/com/lumen1024/data/implementation/repository/FirebaseGroupRepository.kt
@@ -5,12 +5,9 @@ import com.google.firebase.firestore.firestore
 import com.lumen1024.data.dto.GroupDto
 import com.lumen1024.data.dto.toGroup
 import com.lumen1024.data.dto.toGroupDto
-import com.lumen1024.data.toRepositoryObjectChange
 import com.lumen1024.data.tryCatchDerived
 import com.lumen1024.domain.FlowList
 import com.lumen1024.domain.data.Group
-import com.lumen1024.domain.data.RepositoryObjectChange
-import com.lumen1024.domain.data.transform
 import com.lumen1024.domain.repository.GroupRepository
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -80,45 +77,5 @@ class FirebaseGroupRepository @Inject constructor(
     ): Result<Unit> {
         val task = collection.document(groupId).update(data)
         return tryCatchDerived("Failed update group") { task.await() }
-    }
-
-    override fun listenChanges(
-        groupIds: List<String>, callback: (List<RepositoryObjectChange<Group>>) -> Unit
-    ): Result<() -> Unit> {
-        if (groupIds.isEmpty())
-            return Result.failure(Exception("Can't listen changes, groupIds must not be empty"))
-
-        val query = collection.whereIn("id", groupIds)
-
-        return tryCatchDerived("Can't listen changes") {
-            val registration = query.addSnapshotListener { snapshot, e ->
-                if (e != null) return@addSnapshotListener
-
-                val changes = snapshot?.documentChanges?.mapNotNull { firebaseChange ->
-                    val changesDto = firebaseChange.toRepositoryObjectChange<GroupDto>()
-                        ?: return@mapNotNull null
-                    return@mapNotNull changesDto.transform { it.toGroup() }
-                } ?: return@addSnapshotListener
-
-                callback(changes)
-            }
-            return@tryCatchDerived { registration.remove() }
-        }
-    }
-
-    override fun listenChanges(
-        groupId: String, callback: (Group) -> Unit
-    ): Result<() -> Unit> {
-        val query = collection.document(groupId)
-
-        return tryCatchDerived("Can't listen changes") {
-            val registration = query.addSnapshotListener { snapshot, e ->
-                if (e != null) return@addSnapshotListener
-                val data = snapshot?.toObject(GroupDto::class.java)?.toGroup()
-                    ?: return@addSnapshotListener
-                callback(data)
-            }
-            return@tryCatchDerived { registration.remove() }
-        }
     }
 }
