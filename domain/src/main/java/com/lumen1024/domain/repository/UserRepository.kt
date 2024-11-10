6829341b324fd102ca5b36cd4117a60bbd0a3519
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-10 11:45:15 +0700

    ref: UserRepository.kt now return flow

--- domain/src/main/java/com/lumen1024/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/lumen1024/domain/repository/UserRepository.kt
@@ -1,14 +1,10 @@
 package com.lumen1024.domain.repository
 
 import com.lumen1024.domain.data.User
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun getUserById(id: String): Result<User>
+    suspend fun getUserById(id: String): Flow<User?>
     suspend fun addUser(user: User): Result<Unit>
     suspend fun updateUser(id: String, data: Map<String, Any>): Result<Unit>
-
-    fun listenChanges(
-        id: String,
-        callback: (User) -> Unit,
-    ): Result<() -> Unit>
 }
