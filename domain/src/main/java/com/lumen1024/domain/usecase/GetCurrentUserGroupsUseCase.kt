811a67b6081c0bdc9646e29fbaea454ebe89023d
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-11 19:01:41 +0700

    ref: GroupScreen.kt

---
+++ domain/src/main/java/com/lumen1024/domain/usecase/GetCurrentUserGroupsUseCase.kt
@@ -0,0 +1,27 @@
+package com.lumen1024.domain.usecase
+
+import com.lumen1024.domain.FlowList
+import com.lumen1024.domain.data.Group
+import com.lumen1024.domain.data.User
+import com.lumen1024.domain.repository.GroupRepository
+import com.lumen1024.domain.service.AuthService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class GetCurrentUserGroupsUseCase @Inject constructor(
+    private val authService: AuthService,
+    private val groupRepository: GroupRepository,
+) {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): FlowList<Group> = flow {
+        authService.getCurrentUser().flatMapLatest<User?, List<Group>> { user ->
+            if (user == null) return@flatMapLatest flowOf(emptyList())
+
+            groupRepository.getGroupsByIds(user.groups)
+        }
+    }
+}
