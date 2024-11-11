ecd4ce1b370c8a920177456d7c2407952817e78f
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-11 09:23:39 +0700

    ref: hilt modules in app

--- app/src/main/java/com/lumen1024/groupeventer/di/ServiceModule.kt
+++ app/src/main/java/com/lumen1024/groupeventer/di/ServiceModule.kt
@@ -1,6 +1,7 @@
 package com.lumen1024.groupeventer.di
 
 import com.lumen1024.data.implementation.service.FirebaseAuthService
+import com.lumen1024.data.implementation.service.FirebaseAvatarService
 import com.lumen1024.domain.service.AuthService
 import dagger.Binds
 import dagger.Module
@@ -9,8 +10,11 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AuthModule {
+abstract class ServiceModule {
 
     @Binds
     abstract fun bindAuthService(service: FirebaseAuthService): AuthService
+
+    @Binds
+    abstract fun bindAvatarService(service: FirebaseAvatarService): FirebaseAvatarService
 }
