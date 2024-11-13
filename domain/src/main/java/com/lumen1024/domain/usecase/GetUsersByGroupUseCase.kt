3261087bd9d41eb8fb7ada806f0d91ddb12d7ed8
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-13 19:24:42 +0700

    feat: GetUsersByGroupUseCase.kt

---
+++ domain/src/main/java/com/lumen1024/domain/usecase/GetUsersByGroupUseCase.kt
@@ -0,0 +1,27 @@
+package com.lumen1024.domain.usecase
+
+import com.lumen1024.domain.FlowMap
+import com.lumen1024.domain.data.Group
+import com.lumen1024.domain.data.GroupRole
+import com.lumen1024.domain.data.User
+import com.lumen1024.domain.repository.UserRepository
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.transform
+import javax.inject.Inject
+
+class GetUsersByGroupUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+) {
+
+    suspend operator fun invoke(group: Group): FlowMap<User, GroupRole> {
+        val flows = group.members.map { pair ->
+            userRepository.getUserById(pair.key).transform { emit(it to pair.value) }
+        }
+
+        val combined: FlowMap<User, GroupRole> = combine(flows) {
+            it.filter { it.first != null }.map { it.first!! to it.second }.toMap()
+        }
+
+        return combined
+    }
+}
