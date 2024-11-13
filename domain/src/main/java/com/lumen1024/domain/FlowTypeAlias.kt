8fcd5620834beff6cdb59ed2cf273d0f6d06e558
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-11-13 19:26:06 +0700

    feat: FlowMap typealias

--- domain/src/main/java/com/lumen1024/domain/FlowTypeAlias.kt
+++ domain/src/main/java/com/lumen1024/domain/FlowTypeAlias.kt
@@ -5,3 +5,4 @@ import kotlinx.coroutines.flow.Flow
 typealias FlowResult<T> = Flow<Result<T>>
 typealias FlowResultList<T> = Flow<Result<List<T>>>
 typealias FlowList<T> = Flow<List<T>>
+typealias FlowMap<K, V> = Flow<Map<K, V>>
